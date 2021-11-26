b3cd56c2a873d1ce49086d21c57210cf364952a9
aalvarezbretana <aalvarezbretana@danielcastelao.org>
2021-11-26 11:22:16 +0100

    actualizado en clase

---
+++ html/wp-content/themes/newFancy/404.php
@@ -0,0 +1,37 @@
+<?php
+get_header()
+?>
+
+<div id = "Título">
+    <h1>Error en la página</h1>
+</div>
+
+
+<div id = "google">
+
+
+
+</div>
+
+
+<div id = "contenedor">
+    <div id = "cabecera" class="rojo">
+        <h1>Error encontrado</h1>
+    </div>
+    <div id = "cuerpo">
+        <h1></h1>
+
+    </div>
+
+</div>
+
+
+<?php
+get_footer();
+?>
+<div id = "google">
+
+    <h1>
+        <a href = https://www.google.com/>Solucion para arreglarlo</a>
+    </h1>
+</div>
--- html/wp-content/themes/newFancy/header.php
+++ html/wp-content/themes/newFancy/header.php
@@ -1,1 +1,5 @@
-<?php
+<div id = "cabeceras">
+
+    <h1>Encabezado de la página</h1>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+</div>
