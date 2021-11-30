f035e3349f211f1ccd425beae25a94bd8ec92731
aalvarezbretana <aalvarezbretana@danielcastelao.org>
2021-11-30 10:47:32 +0100

    modificacion para que quede la imagen a la izquierda

--- html/wp-content/themes/newFancy/index.php
+++ html/wp-content/themes/newFancy/index.php
@@ -1,19 +1,29 @@
-<?php define( 'WP_USE_THEMES', false ); get_header(); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); the_post_thumbnail(); ?>
-    <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-    <small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
-    <div class="entry">
-        <?php the_excerpt(); ?>
+<?php define('WP_USE_THEMES', false);
+get_header(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div id="estilo">
+        <div id="imagen">
+            <?php the_post_thumbnail(); ?>
+        </div>
+        <div>
+            <h2><a href="<?php the_permalink(); ?>" rel="bookmark"
+                   title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+            <small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
+            <div class="entry">
+                <?php the_excerpt(); ?>
+            </div>
+        </div>
     </div>
+
 <?php endwhile; else : ?>
-    <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
 <?php endif; ?>
 
 <!doctype html>
 <html>
 <head>
 
-    <meta charset = "utf-8">
+    <meta charset="utf-8">
     <title>New Fancy</title>
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 </head>
@@ -21,10 +31,9 @@
 <?php get_header() ?>
 
 
-<div id ="cuerpo">
+<div id="cuerpo">
 
     <?php
-
 
 
     echo "<h1 class = 'morado'>Prueba de un titulo en el body</h1>";
